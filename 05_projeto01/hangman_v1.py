54c94706856a5533be0d4f247fb73fda6062553e
Lenon Alcântara <lenonalcant@hotmail.com>
2023-09-29 11:51:46 -0300

    projeto 01 finalizado

---
+++ 05_projeto01/hangman_v1.py
@@ -0,0 +1,77 @@
+# Projeto 01 - Desenvolvimento de game em Linguagem Python - Versão 1
+
+# Imports
+import random
+from os import system, name
+
+# Função para limpar a tela a cada execução
+def limpa_tela():
+
+  # Windows
+  if name == 'nt':
+    _ = system('cls')
+  
+  # Mac ou Linux
+  else:
+    _ = system('clear')
+
+
+# Função principal
+def game():
+
+  limpa_tela()
+
+  print("\nBem-vindo(a) ao jogo da forca!")
+  print("Adivinhe a palavra abaixo:\n")
+
+  # Lista de palavras para o jogo
+  palavras = ["banana", "abacate", "uva", "morango", "laranja"]
+
+  # Escolhe randomicamente uma palavra
+  palavra = random.choice(palavras)
+
+  # List comprehension
+  letras_descobertas = ['_' for letra in palavra]
+
+  # Número de chances
+  chances = 6
+
+  # Lista para armazenar as letras erradas
+  letras_erradas = []
+
+  # Loop enquanto número de chances for maior do que zero
+  while chances > 0:
+
+    # Print
+    print(" ".join(letras_descobertas))
+    print("\nChances restantes: ", chances)
+    print("Letras erradas: ", " ".join(letras_erradas))
+
+    # Tentativa
+    tentativa = input("\nDigite uma letra: ").lower()
+
+    # Condicional
+    if tentativa in palavra:
+      index = 0
+      for letra in palavra:
+        if tentativa == letra:
+          letras_descobertas[index] = letra
+        index += 1
+    else:
+      chances -= 1
+      letras_erradas.append(tentativa)
+
+    # Condicional
+    if "_" not in letras_descobertas:
+      print("\nVocê venceu, a palavra era:", palavra)
+      break
+
+  # Condicional
+  if "_" in letras_descobertas:
+    print("\nVocê perdeu, a palavra era:", palavra)
+
+# Bloco main
+# Isso indica que este é um programa Python
+if __name__ == "__main__":
+  game()
+  print("\nParabéns. Você está aprendendo programação em Python com a DSA. :)\n")
